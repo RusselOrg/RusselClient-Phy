5ef5947f02d61e269d3a65ee7e0d4ad9cf9425db
safi.saeed1999 <safi.saeed1999@gmail.com>
2024-08-10 20:46:58 +0330

    add async and test and decoder

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="russel-client",
+    name="russelCache-client",
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "requests",
+        "requests","aiohttp"
     ],
-    description="A Python client for interacting with a russel cache server",
+    description="A Python client for interacting with a russelCache cache server",
     author="Saeed Safi",
     author_email="safi.saeed1999@gmail.com",
 )
